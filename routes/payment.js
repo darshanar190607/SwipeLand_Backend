a326467be6d486710835c94a952d2e1d8ab94a99
Darshan AR <ar880darsh@gmail.com>
2025-12-18 11:18:29 +0530

    Initial Backend commit

---
+++ routes/payment.js
@@ -0,0 +1,28 @@
+const express =require("express")
+const Razorpay=require("razorpay")
+
+
+const router = express.Router();
+
+const razorpay = new Razorpay({
+  key_id: process.env.RAZORPAY_KEY_ID,
+  key_secret: process.env.RAZORPAY_KEY_SECRET
+});
+
+router.post("/create-order", async (req, res) => {
+  try {
+    const { amount } = req.body;
+
+    const order = await razorpay.orders.create({
+      amount: amount * 100, // ₹ → paise
+      currency: "INR",
+      receipt: "order_rcptid_11"
+    });
+
+    res.status(200).json(order);
+  } catch (error) {
+    res.status(500).json({ message: "Order creation failed" });
+  }
+});
+
+module.exports=router;
