b2b0f1fb10b3ba80db58835285daa9aef5e0f644
Darshan AR <ar880darsh@gmail.com>
2025-12-18 15:18:42 +0530

    Update CORS origin for production deployment

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ const mongoose=require("mongoose");
 const app=express();
 const cors = require('cors');                                                                        
 app.use(cors({
-  origin: "http://localhost:5173", 
+  origin: "https://swipe-lend-frontend.vercel.app/", 
   methods: ["GET", "POST", "PUT", "DELETE"],
 }));
 app.use(express.json())
