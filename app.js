cd34c2aae5b0e0183288a52ad5f844b0344d1c16
Darshan AR <ar880darsh@gmail.com>
2025-12-19 07:34:14 +0530

    Update app.js

--- app.js
+++ app.js
@@ -24,10 +24,7 @@ const port=process.env.PORT;
 const mongoose=require("mongoose");
 const app=express();
 const cors = require('cors');                                                                        
-app.use(cors({
-  origin: "https://swipe-lend-frontend-git-main-darshanar190607s-projects.vercel.app", 
-  methods: ["GET", "POST", "PUT", "DELETE"],
-}));
+app.use(cors());
 app.use(express.json())
 app.use(helmet())
 
